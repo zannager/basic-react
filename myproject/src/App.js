b8f903dfeba821bc7789501feab81fcdaab92520
zannager <gzraenga@gmail.com>
2024-06-25 02:40:52 +0800

    using state

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
 import Message from './components/Message';
 import Profile from './components/Profile';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Hello />
+      {/* <Hello />
       <Message messageContent="this is a message from props"/>
-      <Profile name="greg" lastname="norman"/>
+      <Profile name="greg" lastname="norman"/> */}
+      <Counter />
     </div>
   );
 }
---
+++ myproject/src/components/Counter.js
@@ -0,0 +1,32 @@
+import { execOnce } from "next/dist/shared/lib/utils";
+import { Component } from "react";
+
+class Counter extends Component {
+    constructor(){
+        super();
+        this.state = {
+            counter: 10,
+        };
+    }
+    // increment(){
+    //     this.setState({
+    //         counter: this.state.counter + 1,
+    //     })
+    // }
+
+    decrement(){
+        this.setState({
+            counter: this.state.counter - 1,
+        })
+    }
+    render() {
+        return (<div>
+            <h3>Count value is: {this.state.counter} </h3>
+            {/* <button onClick={() => this.increment()}>Click</button> */}
+            <button onClick={() => this.decrement()}>Decrement</button>
+        </div>)
+            
+    }
+}
+
+export default Counter;
