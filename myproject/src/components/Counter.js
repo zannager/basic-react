a4700d9dba806aacdf3a9c9d744e8a6ca4407dac
zannager <gzraenga@gmail.com>
2024-06-26 01:53:54 +0800

    event binding

--- myproject/src/components/Counter.js
+++ myproject/src/components/Counter.js
@@ -7,13 +7,19 @@ class Counter extends Component {
         this.state = {
             counter: 0,
         };
-        this.increment = this.increment.bind(this);
+        // this.increment = this.increment.bind(this);
     }
-    increment(){
-        this.setState({
-            counter: this.state.counter + 1,
-        })
-    }
+    // increment(){
+    //     this.setState({
+    //         counter: this.state.counter + 1,
+    //     })
+    // }
+
+    increment = () => { //use arrow function
+            this.setState({
+                counter: this.state.counter + 1,
+            })
+        }
 
     // // decrement(){
     // //     this.setState({
